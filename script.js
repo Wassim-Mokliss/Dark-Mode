f301c5bf2abd1696355a537654b41d4263e1f4c8
Wassim <wasshimu.dev@gmail.com>
2024-11-10 21:57:29 +0100

    Selection + stock element DOM

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('connecté !')
+// Je selectionne et je stocke
+// la DIV switch
+const switchBox = document.querySelector('.switch');
+console.log(switchBox);
+// la DIV btn (le cercle)
+const btn = document.querySelector('.btn');
+console.log(btn);
+// l'icone
+const icone = document.querySelector('i');
+console.log(icone);
+// la DIV container
+const container = document.querySelector('.container');
+console.log(container);
+// le titre
+const titre = document.querySelector('h1');
+console.log(titre);
