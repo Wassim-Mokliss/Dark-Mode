3b335e510e7a74b6edb9dcd25c630958429fb31a
Wassim <wasshimu.dev@gmail.com>
2024-11-10 22:23:47 +0100

    Ajout ecouteur evenement + condition

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ console.log(container);
 // le titre
 const titre = document.querySelector('h1');
 console.log(titre);
+
+// Ajout écouteur evenement
+switchBox.addEventListener('click', function(){
+    console.log('DIV cliquée');
+    btn.classList.toggle('btn-change');
+    icone.classList.toggle('icone-change');
+    icone.classList.toggle('fa-sun');
+    switchBox.classList.toggle('switch-change');
+    container.classList.toggle('container-change');
+    if(titre.innerText === 'Dark-Mode'){
+        titre.innerText = 'Light-Mode'
+    }
+    else{
+        titre.innerText = 'Dark-Mode'
+    }
+});
